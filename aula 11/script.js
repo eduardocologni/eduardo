31164c11f0d82678ae318fde146aa3368ef6dc45
Gui-jpng <delpassoguilherme7@gmail.com>
2024-09-18 09:59:23 -0300

    aula 11

---
+++ aula 11/script.js
@@ -0,0 +1,6 @@
+const numero = document.querySelector('div.numero span')
+const btn_zerar = document.querySelector('.zerar')
+const btn_voltar = document.querySelector('.voltar')
+const btn_avancar = document.querySelector('.avancar')
+
+let contador = 0
