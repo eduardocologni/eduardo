655db97be6bcd8f695cd68a577cdbd2ed5133890
sarah131418 <sarahaureafreitas@gmail.com>
2024-10-09 09:52:49 -0300

    hoje foi a aula 13.2

--- aula13/script.js
+++ aula13/script.js
@@ -30,7 +30,7 @@ const questoes_quiz = [
     b: 'spit alemao',
     c: 'buslaitier',
     d: 'pastor alemao',
-    correta: 'A',
+    correta: 'a',
     },
     {
     questoes: 'quem eu salvaria ',
@@ -70,14 +70,106 @@ const questoes_quiz = [
     b: 'ler tudo pelo menos',
     c: 'tanto faz pq isso nao muda minha vida',
     d: 'tabom',
-    correta: '',
+    correta: 'c',
     },
     {
-    questoes: '',
-    a: '',
-    b: ' ',
-    c: '',
-    d: '',
-    correta: '',
+    questoes: 'em gravy falls qual e o tema principal',
+    a: 'monstros',
+    b: 'suspence',
+    c: 'comedia',
+    d: 'todos a cima',
+    correta: 'd',
     },
 ]
+
+const QUIZ_TITULO = document.querySelector('h1')
+const ALTERNATIVA_A = document.querySelector('#text_a')
+const ALTERNATIVA_B = document.querySelector('#text_b')
+const ALTERNATIVA_C = document.querySelector('#text_c')
+const ALTERNATIVA_D = document.querySelector('#text_d')
+const BOTAO = document.querySelector('button')
+const  QUIZ_CONTAINER = document.querySelector('.containers')
+const ALTERNATIVAS = document.querySelectorAll('.resposta')
+
+let questao_atual = 0
+let acertos = 0 
+let verificou = false
+
+window.onload = carregar_quiz
+
+BOTAO.addEventListener('click' , verificar)
+
+
+
+function carregar_quiz() {
+
+    ALTERNATIVAS.forEach((alternativa) => {
+        alternativa.checked = false
+        document.querySelector(`label[for=${alternativa.id}  ]`).classList.remove('correta', 'incoreta')
+
+    })
+
+    const QUESTAO = questoes_quiz[questao_atual]
+    QUIZ_TITULO.innerText = QUESTAO.questoes
+    ALTERNATIVA_A.innerText = QUESTAO.a
+    ALTERNATIVA_B.innerText = QUESTAO.b
+    ALTERNATIVA_C.innerText = QUESTAO.c
+    ALTERNATIVA_D.innerText = QUESTAO.d
+    BOTAO.innerText = 'verificar'
+    verificou = false
+}
+
+function verificar(){
+    if (!verificou){
+        verificar_resposta()
+    }
+    else{
+        proxima_pergunta()
+    }
+}
+
+function pegar_resposta(){
+    let resposta = undefined
+
+    ALTERNATIVAS.forEach((alternativa) => {
+        if  (alternativa.checked) {
+            resposta = alternativa.id
+        }
+        
+    })
+    return resposta
+}
+
+
+function verificar_resposta(){
+    const RESPOSTAS_JOGADOR = pegar_resposta()
+    const RESPOSTAS_CORETAS = questoes_quiz[questao_atual].correta
+   ALTERNATIVAS.forEach((alternativa)  =>{
+    const LABEL = document.querySelector(`label[for=${alternativa.id}]`)
+    if (alternativa.id === RESPOSTAS_CORETAS){
+        LABEL.classList.add('correta')
+    }
+    else if (alternativa.checked) {
+        LABEL.classList.add('incoreta')
+    }
+   })
+
+    if (RESPOSTAS_JOGADOR === RESPOSTAS_CORETAS){
+        acertos++
+    }
+    BOTAO.innerText = 'proxima'
+    verificou = true
+
+}
+
+function proxima_pergunta(){
+    questao_atual++
+    maximo_de_questoes = questoes_quiz.length
+    
+    if (questao_atual < maximo_de_questoes){
+        carregar_quiz()
+    }
+    else{
+        QUIZ_CONTAINER.innerHTML = `<h1 id="questao">voce acertou ${acertos} de ${maximo_de_questoes} perguntas!</h1>`
+    }
+}
